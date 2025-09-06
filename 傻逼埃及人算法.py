0436c154b8717dd529e562f2381ad49695ae7c3c
Redtone15 <ruihang0523@gmail.com>
2025-09-06 12:14:32 +0800

    Update 傻逼埃及人算法.py
    Improved readability of script with notations

--- 傻逼埃及人算法.py
+++ 傻逼埃及人算法.py
@@ -1,34 +1,37 @@
-
-a = int(input("1\n"))
-b = int(input("2\n"))
-
+#Acqire Two Numbers to be Multiplied
+a = int(input("First number\n"))
+b = int(input("Second number\n"))
 c = 0
-rem =a
-
+Remaining =a
+#Finding out the number of bits needed to store input one
 while a > 1:
     a = a // 2
     c += 1
 comp = 1
 div = [1]
-print("bit needed",c)
+
+print("bit(s) needed",c)
+#listing out the exponents
 for i in range(c):
     comp *= 2
     div.append(comp)
 print("list of exponents of 2",div)
+#finding out the numbers needed to form input a
 plc = []
 cnt = len(div)-1
 while cnt >= 0:
-    if div[cnt] <= rem:
+    if div[cnt] <= Remaining:
         plc.append(div[cnt])
-        rem -= div[cnt]
+        Remaining -= div[cnt]
         cnt -=1
     else:
         cnt -= 1
 print("numbers that form input",a,plc)
-
+#Adding each number after multiplying exponent with input b
 calc=0
 for ans in range(len(plc)):
+    print(b, "*", plc[ans], "=", b*plc[ans])
     calc=calc+b*plc[ans]
-    print (calc,"\n")
+
+    print ("Sum of previous numbers",calc,"\n")
 print("answer is",calc)
-    
